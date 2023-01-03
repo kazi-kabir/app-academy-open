dd17e8b3772a0d2e96db7ab0426bb8435663bfa6
kazi-kabir <kazi.s221999@gmail.com>
2023-01-04 00:54:56 +1100

    update control flow

--- javascript-fundamentals/control-flow.js
+++ javascript-fundamentals/control-flow.js
@@ -6,3 +6,14 @@ function isFive(num) {
 
 console.log(isFive(5)); // => true
 console.log(isFive(13)); // => false
+
+//isOdd
+function isOdd(num) {
+    // your code here!
+    if(num%2 === 0) return false;
+    return true;
+  }
+
+  console.log(isOdd(2)); // => false
+console.log(isOdd(5)); // => true
+console.log(isOdd(-17)); // => true
