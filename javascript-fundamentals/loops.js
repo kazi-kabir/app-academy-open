328b5f26ddeb7288f6c8da4b1a34eb027231074b
kazi-kabir <kazi.s221999@gmail.com>
2023-01-09 19:40:13 +1100

    loop

--- javascript-fundamentals/loops.js
+++ javascript-fundamentals/loops.js
@@ -1,3 +1,10 @@
+function logBetween(lowNum, highNum) {
+    for (let i = lowNum; i <= highNum; i += 1) {
+      console.log(i);
+    }
+  }
+  
+
 function printFives(max) {
     for(let i = 1; i < max; i++) {
       if(i % 5 == 0) {
