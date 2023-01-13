3b7ce25e0e6df6ba265167728d2f8effc89df63c
kazi-kabir <kazi.s221999@gmail.com>
2023-01-13 21:08:34 +1100

    vowel counter func

--- javascript-fundamentals/arrays.js
+++ javascript-fundamentals/arrays.js
@@ -2,3 +2,17 @@ function combineArray(array1, array2) {
     let array3 = [];
     return array3 = array1.concat(array2);
 }
+
+function countVowels(word) {
+    // your code here...
+    let count = 0;
+    let vowels = [ "a", "e", "i", "o", "u"];
+    
+    for(letter in word) {
+      if(vowels.includes(word[letter])) {
+        count++;
+      }
+    }
+  
+    return count;
+};
