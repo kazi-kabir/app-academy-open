72f962b9849ca57666bf87a40994bf9f3d98f7d6
kazi-kabir <117260990+kazi-kabir@users.noreply.github.com>
2023-01-16 19:36:55 +1100

    add doubler method for arrays

--- javascript-fundamentals/arrays.js
+++ javascript-fundamentals/arrays.js
@@ -3,6 +3,16 @@ function combineArray(array1, array2) {
     return array3 = array1.concat(array2);
 }
 
+function doubler(numbers) {
+  let doubledArray = [];
+  for(let i = 0; i < numbers.length; i++) {
+    doubledArray[i] = numbers[i] * 2;
+  
+  }    
+
+  return doubledArray;
+}
+
 function countVowels(word) {
     // your code here...
     let count = 0;
